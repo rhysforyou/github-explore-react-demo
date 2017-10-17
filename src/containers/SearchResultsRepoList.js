b3f9735c97c5fa75f18e8848beb3972fcf4f9f46
Rhys Powell <rhys@rpowell.me>
2017-10-17 18:05:55 +1100

    basic error handling

--- src/containers/SearchResultsRepoList.js
+++ src/containers/SearchResultsRepoList.js
@@ -1,50 +1,14 @@
 import React from 'react'
 import RepoList from '../components/RepoList'
 import debounce from '../util/debounce'
+import defaultResults from './defaultResults.json'
 import './SearchResultsRepoList.css'
 
 const STATE_LOADING = 'loading'
 const STATE_LOADED = 'loaded'
+const STATE_ERROR = 'error'
 
 class SearchResultsRepoList extends React.Component {
-  static defaultResults = [
-    {
-      html_url: 'https://github.com/facebook/react',
-      full_name: 'facebook/react',
-      language: 'JavaScript',
-      description:
-        'A declarative, efficient, and flexible JavaScript library for building user interfaces.',
-      topics: ['javascript', 'react', 'declarative', 'ui', 'library'],
-      homepage: 'https://reactjs.org/'
-    },
-    {
-      html_url: 'https://github.com/rails/rails',
-      full_name: 'rails/rails',
-      language: 'Ruby',
-      description: 'Ruby on Rails',
-      topics: ['rails', 'mvc', 'html', 'activerecord', 'activejob', 'ruby'],
-      homepage: 'http://rubyonrails.org'
-    },
-    {
-      html_url: 'https://github.com/django/django',
-      full_name: 'django/django',
-      language: 'Python',
-      description: 'The Web framework for perfectionists with deadlines',
-      topics: [
-        'python',
-        'django',
-        'web',
-        'framework',
-        'orm',
-        'templates',
-        'models',
-        'views',
-        'apps'
-      ],
-      homepage: 'https://www.djangoproject.com/'
-    }
-  ]
-
   constructor(props) {
     super(props)
 
@@ -83,15 +47,27 @@ class SearchResultsRepoList extends React.Component {
           loadingState: STATE_LOADED
         })
       })
+      .catch(error => {
+        if (searchTerm !== this.props.searchTerm) {
+          return
+        }
+
+        this.setState({
+          repos: [],
+          loadingState: STATE_ERROR
+        })
+      })
   }, 300)
 
   render() {
     if (this.props.searchTerm == null || this.props.searchTerm.length === 0) {
-      return <RepoList repos={SearchResultsRepoList.defaultResults} />
+      return <RepoList repos={defaultResults} />
     } else if (this.state.loadingState === STATE_LOADED) {
       return <RepoList repos={this.state.repos} />
+    } else if (this.state.loadingState === STATE_ERROR) {
+      return <span>Error loading</span>
     } else {
-      return <span class="SearchResultsRepoList__spinner" />
+      return <span className="SearchResultsRepoList__spinner" />
     }
   }
 }
